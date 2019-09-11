d0708efba4c51814963ba4f4dc03680472d364b5
Todd Resudek <toddresudek@gmail.com>
2019-09-10 22:34:26 -0600

    Version 1.0.0

--- gotime.rb
+++ gotime.rb
@@ -2,7 +2,7 @@ class Gotime < Formula
   desc "A command line tool to convert a unix timestamp into a human-readable string"
   homepage "https://github.com/supersimple/gotime"
   url "https://github.com/supersimple/gotime/blob/master/gotime-1.0.0.tar.gz"
-  sha256 "3dcdd4ce482862f7de9ea6eee928e7cced2d85217713ab628ead40e0f56b9bc4"
+  sha256 "0ee442c45ec79eeda74f66d7689d13d41f43452675b66dca5ffa24b21662ae59"
   version "1.0.0"
 
   bottle :unneeded
